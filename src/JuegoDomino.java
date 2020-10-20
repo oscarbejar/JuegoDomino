44a68af0f409f4e4474643a62985d2dee3907712
MatiasMinig <matiasminig@gmail.com>
2020-10-19 21:11:51 -0300

    prueba1

--- src/JuegoDomino.java
+++ src/JuegoDomino.java
@@ -1,6 +1,6 @@
 public class JuegoDomino {
     public static void main(String[] args) {
         System.out.println("Domino");
-        //commit
+        //commit hola estamos probando el jueguito de la profesora
     }
 }
