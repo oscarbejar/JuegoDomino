c95b738d4e7fc716473e8fe82f17d8d693e4ac14
OscarBejar <oskrbejar@gmail.com>
2020-10-19 21:22:12 -0300

    agregada nuevo inicio

--- src/JuegoDomino.java
+++ src/JuegoDomino.java
@@ -1,6 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+
 public class JuegoDomino {
     public static void main(String[] args) {
         System.out.println("Domino");
+        Inicio dib = new Inicio();
+        dib.iniciar();
         //commit
+        /*Ventana vent = new Ventana();
+        vent.ventana.setVisible(true);
+        vent.panelInicio.setVisible(true);
+    //hola
+        /*
+        MarcoConDibujos marco = new MarcoConDibujos();
+        marco.setVisible(true);
+        marco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+*/
+
+
+
+        /*
+
+*/
+
+
+
+
+
+
     }
 }
