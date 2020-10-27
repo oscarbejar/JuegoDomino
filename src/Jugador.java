5c85164468b5cd02bee5cc5803cebc5d7698af3e
MatiasMinig <matiasminig@gmail.com>
2020-10-26 23:07:23 -0300

    comit de mejoras

--- src/Jugador.java
+++ src/Jugador.java
@@ -21,32 +21,7 @@ public class Jugador {
 
 
 }
-/*
 
-    public boolean darFicha(Fichas ficha) {
-        if (ficha != null) {
-            array.add(ficha);
-            return true;
-        } else {
-            return false;
-        }
-    }
-}
-
-
-/*
-    public boolean equals(Object x){
-/*if (!(x instanceof Jugador)) return false;
-int c=0;
-for(int j=0;j<((Jugador)x).fichas.size();j++)
-for(int i=0;i<fichas.size();i++)
-if (fichas.get(i).equals(((Jugador)x).fichas.get(j))) c++;
-return (c==fichas.size()&&
-        fichas.size()==((Jugador)x).fichas.size());
-        return nombre.equals(((Jugador)x).nombre);
-
-    }
-    */
 
 
 
